07c1531f43d3baf0c269771963a38244a37eb770
Enes Alili <e.alili@ineting.net>
2023-10-30 16:24:44 +0100

    Add task count

--- src/views/dashboard/dashboard.component.jsx
+++ src/views/dashboard/dashboard.component.jsx
@@ -1,39 +1,44 @@
-import {DashboardContainer, DashboardItem} from "./dashboard.styles.jsx";
-import {useEffect, useState} from "react";
+import { DashboardContainer, DashboardItem } from "./dashboard.styles.jsx";
+import { useEffect, useState } from "react";
 import axiosClient from "../../axios-client.js";
 
 const Dashboard = () => {
-    const [insights, setInsights] = useState(null)
+  const [insights, setInsights] = useState(null);
 
-    useEffect(() => {
-        fetchInsights()
-    }, []);
+  useEffect(() => {
+    fetchInsights();
+  }, []);
 
-    const fetchInsights = async () => {
-        const response = await axiosClient.get('insights')
-        const insights = await response.data
-        setInsights(insights)
-    }
+  const fetchInsights = async () => {
+    const response = await axiosClient.get("insights");
+    const insights = await response.data;
+    setInsights(insights);
+  };
 
-    return (<>
-            {insights && (<DashboardContainer>
-                    <DashboardItem>
-                        <h2>{insights.clients_count}</h2>
-                        <h2>Clients</h2>
-                    </DashboardItem>
-                    <DashboardItem>
-                        <h2>{insights.projects_count}</h2>
-                        <h2>Projects</h2>
-                    </DashboardItem>
-                    <DashboardItem>
-                        <h2>{insights.users_count}</h2>
-                        <h2>Users</h2>
-                    </DashboardItem>
-                </DashboardContainer>
+  return (
+    <>
+      {insights && (
+        <DashboardContainer>
+          <DashboardItem>
+            <h2>{insights.clients_count}</h2>
+            <h2>Clients</h2>
+          </DashboardItem>
+          <DashboardItem>
+            <h2>{insights.projects_count}</h2>
+            <h2>Projects</h2>
+          </DashboardItem>
+          <DashboardItem>
+            <h2>{insights.users_count}</h2>
+            <h2>Users</h2>
+          </DashboardItem>
+          <DashboardItem>
+            <h2>{insights.tasks_count}</h2>
+            <h2>Tasks</h2>
+          </DashboardItem>
+        </DashboardContainer>
+      )}
+    </>
+  );
+};
 
-            )}
-        </>
-    );
-}
-
-export default Dashboard
+export default Dashboard;
